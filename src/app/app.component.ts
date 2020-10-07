41e8aeaadb9c2808fa8130e9021533c03ae512bd
Max Gomes <maxgomes92@live.com>
2020-10-07 20:57:22 +0200

    Add my-lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyLibService } from 'my-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-project';
+
+  constructor (
+    public myLibService: MyLibService,
+  ) {
+
+  }
+
+  ngOnInit () {
+    console.log('From Lib', this.myLibService.getTime())
+  }
 }
